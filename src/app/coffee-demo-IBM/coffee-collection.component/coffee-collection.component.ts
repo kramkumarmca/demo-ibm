35dc214805bdae0c2b1e18b6ab4d1f0a5548ee74
kramkumarmca <kramkumar.mca@gmail.com>
2022-01-21 11:48:20 -0400

    Design Update

--- src/app/coffee-demo-IBM/coffee-collection.component/coffee-collection.component.ts
+++ src/app/coffee-demo-IBM/coffee-collection.component/coffee-collection.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild, OnInit } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Coffee } from '../coffee-demo-IBM-model';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -8,6 +8,7 @@ import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'coffee-collection',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './coffee-collection.component.html',
   styleUrls: ['./coffee-collection.component.scss'],
 })
@@ -24,16 +25,15 @@ export class CoffeeCollectionComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   
   @ViewChild(MatSort) sort: MatSort;
-  dataSource = new MatTableDataSource<Coffee[]>();
+  dataSource = new MatTableDataSource<Array<Coffee>>();
 
 
   @Input() coffees: Array<Coffee> = [];
   @Output() remove = new EventEmitter<Coffee>();
 
   constructor() {
-
     this.dataSource.paginator = this.paginator;
-
+    console.log(this.coffees)
   }
 
   ngOnInit() {
@@ -43,5 +43,7 @@ export class CoffeeCollectionComponent implements AfterViewInit, OnInit {
   ngAfterViewInit() {
     setTimeout(() => this.dataSource.paginator = this.paginator, 300);
   }
+
+ 
 
 }
